e301bf6be4abd70072570a36f4fb06bdda40d1a8
Seaver Wong <xing.fi@gmail.com>
2018-11-01 11:37:22 +0100

    Minor rails app generation correction

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-const { environment } = require('@rails/webpacker')
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
-  })
-)
-module.exports = environment
