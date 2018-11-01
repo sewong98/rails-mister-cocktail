5f63d525c97b0f98a351abe6aaeb3eaa3bd9f28a
Seaver Wong <xing.fi@gmail.com>
2018-11-01 11:23:08 +0100

    Finished Rails app generation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+module.exports = environment
